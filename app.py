e46e08e770d7cecea1acaaeae024539f7cbbef65
crimson-catfish <sava_kto@mail.ru>
2024-03-20 15:14:07 +0300

    formatted app

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, request
 
-app = Flask(__name__)
+app: Flask = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def greeting():
-    name = request.args.get('name')
-    message = request.args.get('message')
+    name: str = request.args.get("name")
+    message: str = request.args.get("message")
     return f"<h1>Hello {name}! {message}</h1>"
 
-if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(debug=False, host="0.0.0.0")
